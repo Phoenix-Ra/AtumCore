cfe956a31540e6e77dadda0cf16aa29de7657d54
FenixRa22 <53151806+FenixRa22@users.noreply.github.com>
2023-01-06 01:56:53 +0100

    circle effect for rotation tests

--- atum-core/src/main/kotlin/me/phoenixra/atum/plugin/commands/EffectSubcommand.kt
+++ atum-core/src/main/kotlin/me/phoenixra/atum/plugin/commands/EffectSubcommand.kt
@@ -5,11 +5,13 @@ import me.phoenixra.atum.core.command.AtumSubcommand
 import me.phoenixra.atum.core.command.CommandBase
 import me.phoenixra.atum.core.effects.BaseEffectLocation
 import me.phoenixra.atum.core.effects.BaseEffectVariable
+import me.phoenixra.atum.core.effects.types.DynamicCircle
 import me.phoenixra.atum.core.effects.types.Helix
 import org.bukkit.Color
 import org.bukkit.Particle
 import org.bukkit.command.CommandSender
 import org.bukkit.entity.Player
+import org.bukkit.util.Vector
 
 class EffectSubcommand(
     plugin: AtumPlugin,
@@ -33,7 +35,7 @@ class EffectSubcommand(
 
     override fun onCommandExecute(sender: CommandSender, args: MutableList<String>) {
         val player = sender as Player
-        when(args[0]){
+        when(args[0].lowercase()){
             "helix" -> {
                 val effect = Helix(plugin.effectsManager,BaseEffectLocation(player),
                     Particle.REDSTONE, 1, -1)
@@ -44,6 +46,20 @@ class EffectSubcommand(
                 effect.setRadius(0.8)
                 effect.setParticleDrawPerTick(4)
                 effect.setRadiusFunctionIncrementer(0.5)
+
+                plugin.effectsManager.startEffect(effect)
+            }
+            "circle" -> {
+                val effect = DynamicCircle(plugin.effectsManager, BaseEffectLocation(player.location),
+                    Particle.REDSTONE, 1, 15)
+
+                effect.particleColor = BaseEffectVariable(Color.AQUA)
+                effect.thickness = BaseEffectVariable(0.08)
+                effect.setRadius(0.5)
+                effect.setRadiusMultiplier(1.15)
+                effect.setRotation(Vector(args[1].split(";")[0].toDouble(),
+                    args[1].split(";")[1].toDouble(),
+                    args[1].split(";")[2].toDouble()))
 
                 plugin.effectsManager.startEffect(effect)
             }
