6c61448da3bf59b2b97c875e23c3d088479334a0
FenixRa22 <53151806+FenixRa22@users.noreply.github.com>
2023-01-06 16:21:43 +0100

    added subcommand constructor with permission parameter

--- atum-api/src/main/java/me/phoenixra/atum/core/command/AtumSubcommand.java
+++ atum-api/src/main/java/me/phoenixra/atum/core/command/AtumSubcommand.java
@@ -7,17 +7,31 @@ import org.jetbrains.annotations.NotNull;
 public abstract class AtumSubcommand extends AtumCommand{
     @Getter
     public final CommandBase parent;
+
     /**
-     * Create a new command.
+     * Create a new subcommand.
      *
      * @param plugin      The plugin.
      * @param commandName The name used in execution.
-     * @param parent  the parent of a subcommand
+     * @param permission  The permission
+     * @param parent      The parent of a subcommand
      */
-    protected AtumSubcommand(@NotNull AtumPlugin plugin, @NotNull String commandName, @NotNull CommandBase parent) {
-        super(plugin, commandName, parent.getRequiredPermission());
+    protected AtumSubcommand(@NotNull AtumPlugin plugin, @NotNull String commandName, @NotNull String permission, @NotNull CommandBase parent) {
+        super(plugin, commandName, permission);
         this.setConsoleAllowed(parent.isConsoleAllowed());
         this.setPlayersAllowed(parent.isPlayersAllowed());
         this.parent=parent;
     }
+
+    /**
+     * Create a new subcommand.
+     *
+     * @param plugin      The plugin.
+     * @param commandName The name used in execution.
+     * @param parent      The parent of a subcommand
+     */
+    protected AtumSubcommand(@NotNull AtumPlugin plugin, @NotNull String commandName, @NotNull CommandBase parent) {
+        this(plugin,commandName,parent.getRequiredPermission(),parent);
+    }
+
 }
