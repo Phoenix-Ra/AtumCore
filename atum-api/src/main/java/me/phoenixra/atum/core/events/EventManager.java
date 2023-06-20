eebf5bec2ad4b9d0ec7483189e4db18cfb119882
FenixRa22 <53151806+FenixRa22@users.noreply.github.com>
2023-06-20 19:42:45 +0200

    updated event manager

--- atum-api/src/main/java/me/phoenixra/atum/core/events/EventManager.java
+++ atum-api/src/main/java/me/phoenixra/atum/core/events/EventManager.java
@@ -1,17 +1,42 @@
 package me.phoenixra.atum.core.events;
 
 import me.phoenixra.atum.core.AtumPlugin;
+import org.bukkit.event.Event;
 import org.bukkit.event.Listener;
 import org.jetbrains.annotations.NotNull;
 
+import java.util.Set;
+
 public interface EventManager {
 
+    void callEvent(@NotNull Event event);
+
+    /**
+     * Register event listener
+     *
+     * @param listener The listener
+     */
     void registerListener(@NotNull Listener listener);
 
-
+    /**
+     * Unregister event listener
+     *
+     * @param listener The listener
+     */
     void unregisterListener(@NotNull Listener listener);
 
+    /**
+     * Unregister all event listeners
+     *
+     */
     void unregisterAllListeners();
+
+    /**
+     * Get the set of registered event listeners
+     *
+     * @return the listeners set
+     */
+    @NotNull Set<Listener> getRegisteredListeners();
 
     /**
      * Get the plugin.
--- atum-core/src/main/kotlin/me/phoenixra/atum/craft/event/AtumEventManager.kt
+++ atum-core/src/main/kotlin/me/phoenixra/atum/craft/event/AtumEventManager.kt
@@ -3,13 +3,20 @@ package me.phoenixra.atum.craft.event
 import me.phoenixra.atum.core.AtumPlugin
 import me.phoenixra.atum.core.events.EventManager
 import org.bukkit.Bukkit
+import org.bukkit.event.Event
 import org.bukkit.event.HandlerList
 import org.bukkit.event.Listener
 
 class AtumEventManager(private val plugin : AtumPlugin) :
     EventManager {
+    private val pluginManager = Bukkit.getPluginManager()
+    private val registry: Set<Listener> = HashSet()
+    override fun callEvent(event: Event) {
+        pluginManager.callEvent(event)
+    }
+
     override fun registerListener(listener: Listener) {
-        Bukkit.getPluginManager().registerEvents(listener, plugin)
+        pluginManager.registerEvents(listener, plugin)
     }
 
     override fun unregisterListener(listener: Listener) {
@@ -18,6 +25,10 @@ class AtumEventManager(private val plugin : AtumPlugin) :
 
     override fun unregisterAllListeners() {
         HandlerList.unregisterAll(plugin)
+    }
+
+    override fun getRegisteredListeners(): Set<Listener> {
+        return registry;
     }
 
     override fun getPlugin(): AtumPlugin {
