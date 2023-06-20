24d9244ca0789c5cfc886633086b61a98e9eda75
FenixRa22 <53151806+FenixRa22@users.noreply.github.com>
2023-06-21 00:31:35 +0200

    trying to fix jitpack error x3

--- build.gradle.kts
+++ build.gradle.kts
@@ -121,7 +121,6 @@ allprojects {
         }
 
         java {
-            toolchain.languageVersion.set(JavaLanguageVersion.of(17))
             sourceCompatibility = JavaVersion.VERSION_17
             targetCompatibility = JavaVersion.VERSION_17
             withSourcesJar()
