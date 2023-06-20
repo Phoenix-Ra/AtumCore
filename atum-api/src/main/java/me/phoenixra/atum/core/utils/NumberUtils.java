dd8e1ab3b1ec763ff47d2979df0d3ad5a4ffb490
FenixRa22 <53151806+FenixRa22@users.noreply.github.com>
2023-06-20 20:04:44 +0200

    updated numberUtils

--- atum-api/src/main/java/me/phoenixra/atum/core/utils/NumberUtils.java
+++ atum-api/src/main/java/me/phoenixra/atum/core/utils/NumberUtils.java
@@ -1,12 +1,46 @@
 package me.phoenixra.atum.core.utils;
 
-import me.phoenixra.atum.core.exceptions.AtumException;
 import org.jetbrains.annotations.NotNull;
 
+import java.text.DecimalFormat;
+import java.util.Map;
+import java.util.TreeMap;
+
 public class NumberUtils {
-    private NumberUtils() {
-        throw new UnsupportedOperationException("This is an utility class and cannot be instantiated");
+    private static final TreeMap<Integer, String> NUMERALS = new TreeMap<>();
+    private static DecimalFormat doubleFormatter = new DecimalFormat("0.00");
+
+    static {
+        NUMERALS.put(1000, "M");
+        NUMERALS.put(900, "CM");
+        NUMERALS.put(500, "D");
+        NUMERALS.put(400, "CD");
+        NUMERALS.put(100, "C");
+        NUMERALS.put(90, "XC");
+        NUMERALS.put(50, "L");
+        NUMERALS.put(40, "XL");
+        NUMERALS.put(10, "X");
+        NUMERALS.put(9, "IX");
+        NUMERALS.put(5, "V");
+        NUMERALS.put(4, "IV");
+        NUMERALS.put(1, "I");
     }
+
+    /**
+     * Format double to string.
+     * <p></p>
+     * format: 0.00
+     *
+     * @param toFormat The number to format.
+     * @return result.
+     */
+    @NotNull
+    public static String format(final double toFormat) {
+        String formatted = doubleFormatter.format(toFormat);
+
+        return formatted.endsWith("00") ? String.valueOf((int) toFormat) : formatted;
+    }
+
     /**
      * Get if the string can be parsed to a number
      *
@@ -15,7 +49,49 @@ public class NumberUtils {
      * @return if string is a number
      */
     public static boolean isNumber(@NotNull String value, boolean allowPoint){
-        if(allowPoint) return value.matches("[0-9]+\\.[0-9]+|-[0-9]+\\.[0-9]+|[0-9]+|-[0-9]+");
-        else return value.matches("[0-9]+|-[0-9]+");
+        if(allowPoint) return value.matches("\\d+.\\d+|\\d+|-\\d+.\\d+|-\\d+");
+        else return value.matches("\\d+|-\\d+");
+    }
+
+    /**
+     * Get Roman Numeral from number.
+     *
+     * @param number The number to convert.
+     * @return The number, converted to a roman numeral.
+     */
+    @NotNull
+    public static String toNumeral(final int number) {
+        if (number >= 1 && number <= 4096) {
+            int l = NUMERALS.floorKey(number);
+            if (number == l) {
+                return NUMERALS.get(number);
+            }
+            return NUMERALS.get(l) + toNumeral(number - l);
+        } else {
+            return String.valueOf(number);
+        }
+    }
+
+    /**
+     * Get number from roman numeral.
+     *
+     * @param numeral The numeral to convert.
+     * @return The number, converted from a roman numeral.
+     */
+    public static int fromNumeral(@NotNull final String numeral) {
+        if (numeral.isEmpty()) {
+            return 0;
+        }
+        for (Map.Entry<Integer, String> entry : NUMERALS.descendingMap().entrySet()) {
+            if (numeral.startsWith(entry.getValue())) {
+                return entry.getKey() + fromNumeral(numeral.substring(entry.getValue().length()));
+            }
+        }
+        return 0;
+    }
+
+
+    private NumberUtils() {
+        throw new UnsupportedOperationException("This is an utility class and cannot be instantiated");
     }
 }
